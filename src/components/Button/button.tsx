8376b691b6a1c7538364e104b62e8c23307daa98
lmz <lmz@B-Q1M4MD6R-0318.local>
2022-07-12 20:41:36 +0800

    fix:button

---
+++ src/components/Button/button.tsx
@@ -0,0 +1,52 @@
+import React, {AnchorHTMLAttributes, ButtonHTMLAttributes, FC} from "react";
+import classNames from 'classnames';
+
+type ButtonSize = 'large' | 'default' | 'small';
+type ButtonType = 'warning' | 'dannger' | 'default' | 'link';
+
+interface BaseButtonProps {
+  className?: string;
+  disabled?: boolean;
+  children?: React.ReactNode;
+  href?: string;
+  size?: ButtonSize;
+  btnType?: ButtonType;
+}
+
+type NativeButtonProps = ButtonHTMLAttributes<HTMLElement> & BaseButtonProps;
+type AnchorButtonProps = AnchorHTMLAttributes<HTMLElement> & BaseButtonProps;
+export type ButtonProps = Partial<NativeButtonProps & AnchorButtonProps> 
+
+export const Button: FC<ButtonProps>  = (props) => {
+  const {className, disabled, children, href, size, btnType, ...restProps} = props;
+  const classes = classNames('btn', className, {
+    [`btn-${btnType}`]: btnType,
+    [`btn-${size}`]: size,
+    'disabled': (btnType === 'link') && disabled
+  })
+  if (btnType === 'link') {
+    return <a
+    className={classes}
+    href={href}
+    {...restProps}
+    >
+      {children}
+    </a> 
+  } else {
+    return <button
+    className={classes}
+    disabled={disabled}
+    {...restProps}
+    >
+      {children}
+    </button>
+  }
+}
+
+Button.defaultProps = {
+  disabled: false,
+  btnType: 'default',
+  size: 'default',
+}
+
+export default Button;
